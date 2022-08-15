92cd9fda6cff7ca72d9125fde4b996892f37cca9
Pavel Anhur <pavelanhur@gmail.com>
2022-08-15 08:41:17 +0300

    SpringTest without verification

--- src/main/resources/schema-postgres.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS product_spring;
-CREATE TABLE product_spring
-(
-    id          integer PRIMARY KEY,
-    name        VARCHAR(255),
-    price       float precision,
-    description VARCHAR(255)
-);
